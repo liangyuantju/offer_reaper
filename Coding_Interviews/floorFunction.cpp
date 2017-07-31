4f2469d2c9c984b701a12ba9b3682726667306d2
liangyuan2 <liangyuan2@guazi.com>
2017-07-31 16:37:31 +0800

    我的修改

--- Coding_Interviews/floorFunction.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<math.h>
-using namespace std;
-
-int main()
-{
-    int a = floor(double(41/2)+0.5);
-    cout<<a<<endl;
-}
--- Coding_Interviews/quicksort.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-
-void quickSort(int s[], int l, int r)
-{
-    if (l < r)
-}
