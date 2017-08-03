64249f9a9088b970ab1c21e5fd1dfe5087e5e883
liangyuan2 <liangyuan2@guazi.com>
2017-08-03 20:54:12 +0800

    question

---
+++ JianZhi-offer/05_stackToQueue.cpp
@@ -0,0 +1,17 @@
+//题目描述
+//用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+class Solution
+{
+public:
+    void push(int node) {
+
+    }
+
+    int pop() {
+
+    }
+
+private:
+    stack<int> stack1;
+    stack<int> stack2;
+};
